9008da3b95814c1d6c50d1dca927a9917ce3f563
akmalhusain <70431817+akmalhusain@users.noreply.github.com>
2021-12-16 15:25:15 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Data.SqlClient;
+
+
+namespace showdatainconsol
+{
+    class Program
+    
+    {
+        static void Main(string[] args)
+        {
+            Program.Connection();
+            Console.ReadLine();
+        }
+        static void Connection()
+        {
+            SqlConnection con = null;
+            try
+            {
+                con = new SqlConnection("data source=VMDESKTOP-X6232\\MSSQLSERVER01; database=bookdatabase; integrated security=true");
+
+                SqlCommand cm = new SqlCommand("select *from book_data", con);
+                con.Open();
+                SqlDataReader sdr = cm.ExecuteReader();
+                while (sdr.Read())
+                {
+                    Console.WriteLine(sdr["bookID"] + " " + sdr["bookName"]+" "+sdr["authorName"]);
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                con.Close();    
+            }
+        }
+    }
+}
